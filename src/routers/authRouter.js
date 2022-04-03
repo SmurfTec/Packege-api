f2349317fd79ceab6b7e54de706bdf7ba470c8fe
muhammadzain8 <muhammadzain8@gmail.com>
2022-04-04 04:46:58 +0500

    fixes

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -8,9 +8,10 @@ router.post('/signUp', authController.signup);
 router.post('/logIn', authController.login);
 router.post('/sociallogin', authController.socialLogin);
 
-router.route('/confirmMail/:activationLink').get(authController.confirmMail);
-router.route('/forgotPassword').patch(authController.forgotPassword);
 router.route('/update-password').patch(protect, authController.updatePassword);
+
+router.route('/forgotPassword').post(authController.forgotPassword);
+router.route('/confirmMail/:activationLink').get(authController.confirmMail);
 router.route('/resetPassword/:resetToken').patch(authController.resetPassword);
 
 module.exports = router;
